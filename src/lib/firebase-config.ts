c2fbd8e68d9c48a8e92587e25b90946c1133d74a
Lance Lavandowska <lavandowska@gmail.com>
2025-08-18 19:06:58 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -4,6 +4,6 @@ export const firebaseConfig = {
   "appId": "1:595646494295:web:688a54c45770ea3f63d512",
   "storageBucket": "brainopolisbooks-ptjrj.firebasestorage.app",
   "apiKey": "AIzaSyBa75E6Pn-u2JXFRKZKV4LPeXAWC6-S6pk",
-  "authDomain": "brainopolis-books--brainopolisbooks-ptjrj.us-central1.hosted.app",
+  "authDomain": "brainopolisbooks-ptjrj.firebaseapp.com",
   "messagingSenderId": "595646494295"
 };
