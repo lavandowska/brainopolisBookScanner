3f027f327362786c22522b7d2376583b5c9ca484
Lance Lavandowska <lavandowska@gmail.com>
2025-08-26 17:36:28 +0000

    what am i missing to successfully deploy firebase functions?

--- src/functions/index.js
+++ src/functions/index.js
@@ -8,45 +8,51 @@ const cors = require('cors');
 admin.initializeApp();
 const app = express();
 
+// Define Stripe secret keys using Firebase's secret management
+const stripeToken = defineString("stripe_token");
+const paymentsWebhookSecret = defineString("payments_webhook_secret");
+
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
 // Add middleware to parse raw body for Stripe signature verification
 app.use(express.json({ verify: (req, res, buf) => { req.rawBody = buf } }));
 
-exports.stripeWebhook = onRequest(async (req, res) => {
-  const stripeConfig = (functions.config ? functions.config().stripe : defineString("stripe_token"));
-  const stripe = require("stripe")(stripeConfig.token);
-  let event;
+exports.stripeSessionCompletedWebhook = onRequest(
+  // Make secrets available to the function
+  { secrets: ["stripe_token", "payments_webhook_secret"] },
+  async (req, res) => {
+    const stripe = require("stripe")(stripeToken.value());
+    let event;
 
-  try {
-    const whSec = stripeConfig.payments_webhook_secret;
-    event = stripe.webhooks.constructEvent(
-      req.rawBody,
-      req.headers["stripe-signature"],
-      whSec,
-    );
-  } catch (err) {
-    console.error("⚠️ Webhook signature verification failed.", err);
-    return res.sendStatus(400);
-  }
+    try {
+      const whSec = paymentsWebhookSecret.value();
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        req.headers["stripe-signature"],
+        whSec,
+      );
+    } catch (err) {
+      console.error("⚠️ Webhook signature verification failed.", err);
+      return res.sendStatus(400);
+    }
 
-  // Handle the event
-  if (event.type === 'checkout.session.completed') {
-    const dataObject = event.data.object;
-    console.log("Checkout session completed:", JSON.stringify(dataObject));
-    await admin.firestore().collection("orders").add({
-      checkoutSessionId: dataObject.id,
-      paymentStatus: dataObject.payment_status,
-      shippingInfo: dataObject.shipping,
-      amountTotal: dataObject.amount_total,
-      userId: dataObject.userId
-    });
-    creditsPurchased(dataObject.userId, dataObject.amount_total)
-  
-    return res.sendStatus(200);
-  }
+    // Handle the event
+    if (event.type === 'checkout.session.completed') {
+      const dataObject = event.data.object;
+      console.log("Checkout session completed:", JSON.stringify(dataObject));
+      await admin.firestore().collection("orders").add({
+        checkoutSessionId: dataObject.id,
+        paymentStatus: dataObject.payment_status,
+        shippingInfo: dataObject.shipping,
+        amountTotal: dataObject.amount_total,
+        userId: dataObject.userId
+      });
+      creditsPurchased(dataObject.userId, dataObject.amount_total)
+    
+      return res.sendStatus(200);
+    }
 
-  // Return a response to acknowledge receipt of the event
-  res.sendStatus(200);
+    // Return a response to acknowledge receipt of the event
+    res.sendStatus(200);
 });
